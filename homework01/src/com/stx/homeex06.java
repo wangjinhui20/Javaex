a6ca4861032af18ed0d8053bbe3d6f78b6cae246
wang <xxibystander@gmail.com>
2021-10-16 21:52:08 +0800

    Initial commit

--- homework01/src/com/stx/homeex06.java
+++ homework01/src/com/stx/homeex06.java
@@ -2,22 +2,31 @@ package com.stx;
 
 public class homeex06 {
     public static void main(String[] args) {
-
+        Circle C= new Circle();
+        PassObject p = new PassObject();
+        p.printAreas(C, 5);
     }
 }
 
 class Circle {
     double r;
 
-    public double findArea(double r) {
+    public double findArea() {
         return 3.14 * r * r;
+    }
+
+    public void setRadius(double r) {
+        this.r = r;
     }
 }
 
+
 class PassObject {
     public void printAreas(Circle c, int times) {
-        for (int i = 0; i < times; i++) {
-            System.out.println(i);
+        for (int i = 0; i <= times; i++) {
+            System.out.println("r\tarea");
+            c.setRadius(i);
+            System.out.println(i + "\t" + c.findArea());
         }
     }
 }
